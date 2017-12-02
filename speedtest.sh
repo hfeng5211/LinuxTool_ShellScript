e1bf13250ad79a4ed0a7c241691348cb05ddf3b9
Marc <hfeng5211@qq.com>
2017-12-02 09:56:06 +0800

    Create speedtest.sh

---
+++ speedtest.sh
@@ -0,0 +1,15 @@
+wget https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+chmod a+rx speedtest.py
+sudo mv speedtest.py /usr/local/bin/speedtest
+sudo chown root:root /usr/local/bin/speedtest
+echo "------------------------------------------------"
+echo "\t  LSB Version:    :core-4.1-amd64:core-4.1-noarch"
+echo "\t  Distributor ID: CentOS"
+echo "\t  Description:    CentOS Linux release 7.3.1611 (Core)"
+echo "\t  Release:        7.3.1611"
+echo "\t  Codename:       Core"
+echo "------------------------------------------------"
+echo "if you can't use,I don't I test  on CentOs above"
+echo "install speedtest ok"
+echo "use speedtest to test your net connect speed"
+
